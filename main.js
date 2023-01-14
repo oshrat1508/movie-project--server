00fbdaf09d4cdf2c08713d3828570121a67e2233
oshrat <oshbalay@gmail.com>
2023-01-14 19:14:10 +0200

    favorite page

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/chilTime',movie_router)
+app.use('/chillTime',movie_router)
 app.use('/',users)
 
 app.listen(8000 , ()=>console.log('listen to port 8000'))
